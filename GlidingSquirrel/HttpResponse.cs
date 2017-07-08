7179a24e07f513d57225a5783188fd8cb7464554
Starbeamrainbowlabs <sbrl@starbeamrainbowlabs.com>
2017-07-08 10:10:28 +0100

    Finish off HttpResponse

--- GlidingSquirrel/HttpResponse.cs
+++ GlidingSquirrel/HttpResponse.cs
@@ -7,20 +7,37 @@ namespace SBRL.GlidingSquirrel
 {
 	public class HttpResponse
 	{
+		public float HttpVersion = 1.0f;
 		public HttpResponseCode ResponseCode = HttpResponseCode.Ok;
 		public Dictionary<string, string> Headers = new Dictionary<string, string>();
 
 		public StreamReader Body;
+		public int ReadBufferSize = 32768;
 
 		public HttpResponse()
 		{
 		}
 
-		public async Task<bool> SendTo(StreamWriter destination)
+		public async Task SendTo(StreamWriter destination)
 		{
-			throw new NotImplementedException();
+			// Write the first line out
+			await destination.WriteAsync(string.Format(
+				"HTTP/{0:0.0} {1} {2}\r\n",
+				HttpVersion,
+				ResponseCode.Code,
+				ResponseCode.Message
+			));
+			// Write the headers out
+			foreach(KeyValuePair<string, string> header in Headers)
+				await destination.WriteAsync($"{header.Key}: {header.Value}\r\n");
 
-			return true;
+			await destination.WriteAsync("\r\n");
+
+			// Use a buffer to send the file in chunks
+			byte[] buffer = new byte[ReadBufferSize];
+			int lastReadSize;
+			while((lastReadSize = await Body.BaseStream.ReadAsync(buffer, 0, ReadBufferSize)) < ReadBufferSize)
+				await destination.BaseStream.WriteAsync(buffer, 0, lastReadSize);
 		}
 	}
 }
