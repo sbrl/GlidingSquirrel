0bbea7944d8dfa7941474b77dc91d03f2925d1c9
Starbeamrainbowlabs <sbrl@starbeamrainbowlabs.com>
2017-08-21 22:24:47 +0100

    Write an initial (untested\!) WebsocketFrame encoder. Next up: dealing with opcodes.

--- GlidingSquirrel/Websocket/WebsocketFrame.cs
+++ GlidingSquirrel/Websocket/WebsocketFrame.cs
@@ -2,6 +2,7 @@
 using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace SBRL.GlidingSquirrel.Websocket
 {
@@ -59,6 +60,65 @@ namespace SBRL.GlidingSquirrel.Websocket
 
 		public WebsocketFrame()
 		{
+		}
+
+		public async Task SendTo(NetworkStream clientStream)
+		{
+			byte[] headerBuffer = new byte[4];
+
+			headerBuffer[0] |= (byte)(Convert.ToByte(Fin) << 7);
+			headerBuffer[0] |= (byte)(Convert.ToByte(Rsv1) << 6);
+			headerBuffer[0] |= (byte)(Convert.ToByte(Rsv2) << 5);
+			headerBuffer[0] |= (byte)(Convert.ToByte(Rsv3) << 4);
+			if(Opcode > 0b1111)
+				throw new InvalidDataException("Error: The opcode value can't be greater than 15 (0b1111)!");
+			headerBuffer[0] |= Convert.ToByte(Opcode);
+
+			headerBuffer[1] |= (byte)(Convert.ToByte(Masked) << 7);
+
+			PayloadLengthType payloadLengthType = PayloadLengthType.Bit7;
+			byte payloadLengthValue = (byte)RawPayload.Length;
+			if(RawPayload.Length > 125 && RawPayload.Length <= 65535)
+			{
+				payloadLengthType = PayloadLengthType.Bit16;
+				payloadLengthValue = 126;
+			}
+			else if(RawPayload.Length > 65535)
+			{
+				payloadLengthType = PayloadLengthType.Bit64;
+				payloadLengthValue = 127;
+			}
+
+			if(payloadLengthValue > 127)
+				throw new InvalidDataException($"Error: That payloadLengthValue of {payloadLengthValue} is invalid.");
+
+			headerBuffer[1] |= payloadLengthValue;
+
+			// Write the main header to the stream
+			await clientStream.WriteAsync(headerBuffer, 0, 2);
+
+			// Write the extended payload length (if any) to the stream
+			if(payloadLengthType == PayloadLengthType.Bit16)
+			{
+				byte[] payloadLength16 = BitConverter.GetBytes((ushort)RawPayload.Length);
+				await clientStream.WriteAsync(payloadLength16, 0, 2);
+			}
+			if(payloadLengthType == PayloadLengthType.Bit64)
+			{
+				byte[] payloadLength64 = BitConverter.GetBytes((ulong)RawPayload.Length);
+				await clientStream.WriteAsync(payloadLength64, 0, 8);
+			}
+
+			// Write the masking key to the stream if needed
+			if(Masked)
+				await clientStream.WriteAsync(MaskingKey, 0, 4);
+
+			await clientStream.FlushAsync();
+
+			// Write the payload to the stream
+			await clientStream.WriteAsync(RawPayload, 0, RawPayload.Length);
+
+			await clientStream.FlushAsync();
 		}
 
 
