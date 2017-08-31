4d64cb8e6d5781fc76ebe53d28f14d7b76dea07b
Starbeamrainbowlabs <sbrl@starbeamrainbowlabs.com>
2017-08-31 12:19:37 +0100

    Write test chat websockets server

---
+++ GlidingSquirrel/Modes/ChatWebsocketServer.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+
+using SBRL.GlidingSquirrel.Http;
+using SBRL.GlidingSquirrel.Websocket;
+using SBRL.Utilities;
+
+namespace SBRL.GlidingSquirrel.Modes
+{
+	public class ChatWebsocketServer : WebsocketServer
+	{
+		public ChatWebsocketServer(IPAddress inBindAddress, int inPort) : base(inBindAddress, inPort)
+		{
+		}
+
+		public override async Task HandleClientConnected(object sender, ClientConnectedEventArgs eventArgs)
+		{
+			WebsocketClient client = eventArgs.ConnectingClient;
+			// Send a welcome message
+			await client.Send(
+				"Welcome to this sample websockets server!<br />\n" +
+				"This server will echo any frames you send it to all other connected clients."
+			);
+
+			// Echo text and binary messages we geet sent
+			client.OnTextMessage += async (object textSender, TextMessageEventArgs textEventArgs) => {
+				await Reflect(textSender as WebsocketClient, textEventArgs.Payload);
+			};
+			client.OnBinaryMessage += async (object binarySender, BinaryMessageEventArgs binaryEventArgs) => {
+				await Reflect(binarySender as WebsocketClient, binaryEventArgs.Payload);
+			};
+		}
+
+		public override Task HandleClientDisconnected(object sender, ClientDisconnectedEventArgs eventArgs)
+		{
+			return Task.CompletedTask;
+		}
+
+		public override async Task HandleHttpRequest(HttpRequest request, HttpResponse response)
+		{
+			if(request.Url != "/")
+			{
+				response.ResponseCode = HttpResponseCode.NotFound;
+				response.ContentType = "text/plain";
+				await response.SetBody("Couldn't find anything at '{request.Url}'.");
+				return;
+			}
+
+			response.ResponseCode = HttpResponseCode.Ok;
+			response.ContentType = "text/html";
+			await response.SetBody(
+				await EmbeddedFiles.ReadAllTextAsync("SBRL.GlidingSquirrel.Modes.Resources.ChatWebsocketClient.html")
+			);
+		}
+
+		protected override Task setup()
+		{
+			return Task.CompletedTask;
+		}
+	}
+}
---
+++ GlidingSquirrel/Websocket/Values.cs
@@ -0,0 +1,50 @@
+﻿using System;
+namespace SBRL.GlidingSquirrel.Websocket
+{
+
+	/// <summary>
+	/// Indicates the method by which a payload's length was specified.
+	/// </summary>
+	public enum PayloadLengthType
+	{
+		Bit7 = 0,
+		Bit16 = 16,
+		Bit64 = 64
+	}
+
+	/// <summary>
+	/// Indicates the a type of websocket frame.
+	/// </summary>
+	public enum WebsocketFrameType
+	{
+		ContinuationFrame = 0x0,
+		TextData = 0x1,
+		BinaryData = 0x2,
+
+		Close = 0x8,
+
+		Ping = 0x9,
+		Pong = 0xA
+	}
+
+	/// <summary>
+	/// Indicates the reason why a websocket connection is beingg closed.
+	/// </summary>
+	public enum WebsocketCloseReason
+	{
+		NotClosedYet = 0,
+		Normal = 1000,
+		Shutdown = 1001,
+		ProtocolError = 1002,
+		NotAcceptableDataType = 1003,
+		Reserved = 1004,
+		NoStatusCodePresent = 1005,
+		AbnormalClosure = 1006,
+		InConsistentDataEncoding = 1007,
+		PolicyViolation = 1008,
+		FrameTooBig = 1009,
+		ExtensionsNotPresent = 1010,
+		UnexpectedCondition = 1011,
+		TlsHandshakeFailure = 1015
+	}
+}
