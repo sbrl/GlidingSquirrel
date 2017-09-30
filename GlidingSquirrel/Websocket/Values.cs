05fa1cfb0ed3457055171c3d682340ce1156fe06
Starbeamrainbowlabs <sbrl@starbeamrainbowlabs.com>
2017-09-30 13:51:17 +0100

    [Websockets] Fix issue with garbage collected close reasons, and comment all the close reasons too

--- GlidingSquirrel/Websocket/Values.cs
+++ GlidingSquirrel/Websocket/Values.cs
@@ -32,19 +32,68 @@ namespace SBRL.GlidingSquirrel.Websocket
 	/// </summary>
 	public enum WebsocketCloseReason
 	{
+		/// <summary>
+		/// The connection hasn't closed yet. You shouldn't ever see this status code when a connection has
+		/// been closed! If you do, then it means there's a _bug_.
+		/// </summary>
 		NotClosedYet = 0,
+		/// <summary>
+		/// The connection was closed normally.
+		/// </summary>
 		Normal = 1000,
+		/// <summary>
+		/// The connection was closed because either or both parties are shutting down / going away.
+		/// </summary>
 		Shutdown = 1001,
+		/// <summary>
+		/// An involved party had an issue with the syntax of a message they received from the other party.
+		/// </summary>
 		ProtocolError = 1002,
+		/// <summary>
+		/// An involved party didn't like the type of frame sent by the other party.
+		/// </summary>
 		NotAcceptableDataType = 1003,
+		/// <summary>
+		/// A reserved close code. Should not be used.
+		/// </summary>
 		Reserved = 1004,
+		/// <summary>
+		/// No status code was specified.
+		/// </summary>
 		NoStatusCodePresent = 1005,
+		/// <summary>
+		/// The connection was closed abnormally - probably due to an exception being thrown somewhere.
+		/// </summary>
 		AbnormalClosure = 1006,
-		InConsistentDataEncoding = 1007,
+		/// <summary>
+		/// A text message was received that doesn't match the encoding that it claims to be written in.
+		/// </summary>
+		InconsistentDataEncoding = 1007,
+		/// <summary>
+		/// A generic code for when a message sent by the other party violates the receiving party's policy.
+		/// </summary>
 		PolicyViolation = 1008,
+		/// <summary>
+		/// A frame was received that's too big for the receiving party to process.
+		/// </summary>
 		FrameTooBig = 1009,
+		/// <summary>
+		/// One or more required extensions weren't negotiated in the initial handshake.
+		/// </summary>
 		ExtensionsNotPresent = 1010,
+		/// <summary>
+		/// The server has encountered an 'unexpected' condition that is preventing it from fulfilling the
+		/// request in the frame received.
+		/// </summary>
 		UnexpectedCondition = 1011,
-		TlsHandshakeFailure = 1015
+		/// <summary>
+		/// The TLS handshake failed. This _shouldn't_ be actually sent to the client - it's for logging
+		/// purposes only.
+		/// </summary>
+		TlsHandshakeFailure = 1015,
+		/// <summary>
+		/// The close reason sent or received has been lost somehow - probably due to garbage collection.
+		/// </summary>
+		CloseReasonLost = 4888
 	}
 }
